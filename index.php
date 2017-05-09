d23285c2711fab43bc9931a6d2a871b3c8944a08
fakekamrankhan <kamran@kamrankhan.ca>
2017-05-09 09:39:41 -0700

    Modifying the convertus database class to store divisions. Also adding prelimiary models retrieval from the chrome api

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php // Silence is golden
 
 include_once( dirname(__FILE__) . '/includes/wpdb.php' );
+include_once( dirname(__FILE__) . '/includes/formatting.php' );
 
 
 class Chrome_Data_API {
@@ -41,17 +42,16 @@ class Chrome_Data_API {
 
 	protected function soap_call( $function, $args = array(), $all_years = false ) {
 
-		$args['accountInfo'] = $this->account_info;
+		$params = $args;
+		$params['accountInfo'] = $this->account_info;
 
-		$soap_response = $this->soap->__soapCall( $function, array( $args ) );
+		$soap_response = $this->soap->__soapCall( $function, array( $params ) );
 
-		if ( $soap_response->responseStatus->responseCode === 'Successful' ) {
+		$response = new stdClass();
+		$response->response = $soap_response;
+		$response->parameters = $args;
 
-			return $soap_response;
-
-		}
-
-		return false;
+		return $response;
 
 	}
 
@@ -87,12 +87,13 @@ class Chrome_Data_API {
 	 *
 	 * @return array The last x years set by $range
 	 */
-	private function get_years( $range = 3 ) {
+	private function get_years( $range = 2 ) {
 
 		$soap_response = $this->soap_call( 'getModelYears' );
+		$response = $soap_response->response;
 
 		return array_slice(
-			$soap_response->modelYear,
+			$response->modelYear,
 			-$range
 		);
 
@@ -122,6 +123,34 @@ class Chrome_Data_API {
 
 	}
 
+	protected function truncate_response_parameters( $data, $args ) {
+
+		$truncated_data = array();
+		$response_data = array_column( $data, 'response' );
+
+		foreach( $response_data as $response ) {
+			$truncated_data[] = $response->{$args['property']};
+		}
+
+		foreach ( $truncated_data as $key => $value ) {
+			if ( is_object( $value ) ) {
+				$truncated_data[ $key ] = array( $value );
+			}
+		}
+
+		if ( array_key_exists( 'unique', $args ) ) {
+			$truncated_data = array_values( 
+				array_unique( 
+					call_user_func_array( 'array_merge', $truncated_data ),
+					SORT_REGULAR 
+				) 
+			);
+		}
+
+		return $truncated_data;
+
+	}
+
 }
 
 Class Convertus_API {
@@ -141,42 +170,117 @@ class Convertus_DB_Updater extends Chrome_Data_API {
 
 	}
 
-	public function update_divisions() {
+	public function get_divisions() {
 
-		$response = $this->soap_call_loop( 'getDivisions' );
+		$soap_response = $this->soap_call_loop( 'getDivisions' );
 
-		$divisions = $this->truncate_response_data( 
-			$response, 
-			array( 
-				'property' => 'division', 
-				'unique' => true 
-			) 
-		);
+		$divisions = array();
 
-		$returned_divisions = array();
-		foreach( $divisions as $index => $object ) {
-			$returned_divisions[] = array( 'name' => $object->_, 'logo' => $object->_ );
+		foreach( $soap_response as $response ) {
+
+			if ( is_array( $response->response->division ) ) {
+
+				foreach( $response->response->division as $division ) {
+
+					$division->name = $division->_;
+					$division->image = 'http://api.convertus.com/assets/logos/' . sanitize_title_with_dashes( $division->_ ) . '.png';
+					unset( $division->_ );
+
+					$divisions[] = $division;
+
+				}
+
+			} else if ( is_object( $response->response->division ) ) {
+
+				$obj = new stdClass();
+
+				$division = $response->response->division;
+				$division->name = $division->_;
+				$division->image = 'http://api.convertus.com/assets/logos/' . sanitize_title_with_dashes( $division->_ ) . '.png';
+				unset( $division->_ );
+				$divisions[] = $response->response->division;
+
+			}
+
 		}
+
+		return array_values( array_intersect_key( $divisions, array_unique( array_column( $divisions, 'id' ) ) ) );
 
 	}
 
-	private function truncate_response_data( $data, $args ) {
+	private function update_divisions() {
 
-		$truncated_data = array_column( $data, $args['property'] );
+		$divisions = $this->get_divisions();
 
-		if ( $args['unique'] ) {
-			$truncated_data = array_values( 
-				array_unique( 
-					call_user_func_array( 'array_merge', $truncated_data ), 
-					SORT_REGULAR 
-				) 
-			);
+		$query = 'INSERT divisions ( division_name, division_id, image ) VALUES ';
+		$sql_values = array();
+
+		foreach( $divisions as $division ) {
+			$sql_values[] = "('{$division->name}', {$division->id}, '{$division->image}')";
+		}
+		$query .= implode( ',', $sql_values );
+
+		$this->db->query( 'TRUNCATE divisions' );
+		$this->db->query( $query );
+
+	}
+
+	public function get_models( $division_id = -1 ) {
+
+		if ( $division_id === -1 ) {
+			$divisions = $this->db->get_results( 'SELECT * FROM divisions' );
 		}
 
-		return $truncated_data;
+		if ( $divisions ) {
+			$soap_response = array();
+			foreach( $divisions as $division ) {
+				$soap_response[] = $this->soap_call_loop( 'getModels', array( 'divisionId' => $division->division_id ) );
+			}
+		}
+
+		$models = array();
+
+		foreach( $soap_response as $first_response ) {
+
+			foreach( $first_response as $response ) {
+
+				if ( $response->response->responseStatus->responseCode === 'Successful' ) {
+
+					if ( is_array( $response->response->model ) ) {
+
+						foreach( $response->response->model as $model ) {
+
+							$model->name = $model->_;
+							$model->year = $response->parameters['modelYear'];
+							$model->division_id = $response->parameters['divisionId'];
+							unset( $model->_ );
+
+							$models[] = $model;
+
+						}
+
+					} else if ( is_object( $response->response->model ) ) {
+
+						$model = $response->response->model;
+						$model->name = $model->_;
+						$model->year = $response->parameters['modelYear'];
+						$model->division_id = $response->parameters['divisionId'];
+						unset( $model->_ );
+						$models[] = $model;
+
+					}
+
+				}
+
+			}
+
+		}
+
+		return $models;
 
 	}
 
 }
 
-new Convertus_DB_Updater( 'CA' );
+$obj = new Convertus_DB_Updater( 'CA' );
+print_r($obj->get_models());
